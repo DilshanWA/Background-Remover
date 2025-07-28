2942245def1ebfaa183dbe8d4a7ad4b5e446065f
Dilshan Thathsara <dilshanpersonaluse@gmail.com>
2025-07-28 21:04:55 +0530

    update leftside
    update leftside

--- client/app/components/ImagePreview.tsx
+++ client/app/components/ImagePreview.tsx
@@ -8,9 +8,18 @@ const ImagePreview = ({ imageUrl }: Props) => {
       {imageUrl ? (
         <img src={imageUrl} alt="Preview" className="rounded-xl object-contain h-full" />
       ) : (
-        <div className="text-gray-400 text-center">
-          <p className="text-lg">Image Preview</p>
+        <div className="text-center my-6">
+          <p className="text-3xl font-extrabold text-gray-800">
+            Remove Image <br /> Background
+          </p>
+          <p className="text-gray-500 text-lg mt-2">
+            100% Automatically and 
+            <span className="bg-yellow-300 text-black font-semibold px-2 py-1 rounded ml-2">
+              Free
+            </span>
+          </p>
         </div>
+
       )}
     </div>
   );
